794d755ae8cb20c678dbe400c5613fb63b979b25
Murilo Oliveira <64328787+Muriloods@users.noreply.github.com>
2021-06-15 23:18:34 +0000

    primeiro execício

--- dolar.c
+++ dolar.c
@@ -3,9 +3,14 @@
 int main(void) {
 
   float real, euro, dolar;
-  printf("Informe o preço da passagem em R$:/n");
-  
+  printf("Informe o preço da passagem em R$:\n");
+  scanf("%f", &real);
 
+  dolar = real * 5.05;
+  euro = real * 6.12;
+
+  printf("O valor da passagem em US$ é %.2f\n", dolar);
+  printf("O valor da passagem em US$ é € %.2f\n", euro);
 
   return 0;
 }
