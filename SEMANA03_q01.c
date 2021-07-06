3bd66d9d19d90a4ed082e1ef267b84e460ac5e09
Murilo Oliveira <64328787+Muriloods@users.noreply.github.com>
2021-07-06 23:25:19 +0000

    exercicios

---
+++ SEMANA03_q01.c
@@ -0,0 +1,16 @@
+/* #include <stdio.h>
+
+int main(void) {
+  int a, b;
+  scanf("%d %d", &a, &b);
+  if(a > b)
+  {
+    printf("%d %d %d %d %d %d\n", b-1, b, b+1, a-1, a, a+1);
+  }
+  else
+  {
+    printf("%d %d %d %d %d %d\n", a-1, a, a+1, b-1, b, b+1);
+  }
+
+  return 0;
+} */
---
+++ SEMANA03_q02.c
@@ -0,0 +1,25 @@
+/* #include <stdio.h>
+
+int main(void) {
+  int a, b, c;
+  scanf("%d %d %d", &a, &b, &c);
+  if(a > (b + c))
+  {
+    printf("Candidato A\n");
+  }
+  else if(b > (a + c))
+  {
+    printf("Candidato B\n");
+  }
+  else if(c > (a + b))
+  {
+    printf("Candidato C\n");
+  }
+  else
+  {
+    printf("SEGUNDO TURNO\n");
+  }
+
+
+  return 0;
+} */
