fb1b8f5537178903e2557f93e518117e4f3785b0
Murilo Oliveira <64328787+Muriloods@users.noreply.github.com>
2021-07-19 22:14:28 +0000

    lista 2

---
+++ SEMANA04_q03.c
@@ -0,0 +1,38 @@
+/* #include <stdio.h>
+
+int main(void) {
+  int a, b;
+  scanf("%d %d", &a, &b);
+  if(a > b)
+  {
+    if(a == b + 1)
+    {
+      printf("%d %d %d %d\n", b - 1, b, a, a + 1);
+    }
+    else if(a == b + 2)
+    {
+      printf("%d %d %d %d %d\n",  b - 1, b, b + 1, a, a + 1);
+    }
+    else
+    {
+      printf("%d %d %d %d %d %d\n", b - 1, b, b + 1, a - 1, a, a + 1);
+    }
+  }
+  else
+  {
+    if(b == a + 1)
+    {
+      printf("%d %d %d %d\n", a - 1, a, b, b + 1);
+    }
+    else if(b == a + 2)
+    {
+      printf("%d %d %d %d %d\n",  a - 1, a, a + 1, b, b + 1);
+    }
+    else
+    {
+      printf("%d %d %d %d %d %d\n", a - 1, a, a + 1, b - 1, b, b + 1);
+    }
+  }
+
+  return 0;
+} */
